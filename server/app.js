3f24831a1a7c1b27f138157a0932deaa3a10f626
windows <edgar.walugembe.upti@gmail.com>
2024-03-27 17:03:21 +0300

    client && server 0003

--- server/app.js
+++ server/app.js
@@ -19,7 +19,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://breakfast-app-production.onrender.com",
     credentials: true,
   })
 );
