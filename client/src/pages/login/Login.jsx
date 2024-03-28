6e512b2e4586afd0de1c1ed9ae98663de549d5b8
windows <edgar.walugembe.upti@gmail.com>
2024-03-28 11:07:03 +0300

    client && server 0003

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -92,7 +92,7 @@ const Login = () => {
           <div className="flex justify-end items-end p-0 gap-10">
             <div className={`rounded forgotSpan mb-2`}>
               <Link
-                to="/password/reset_password/:id"
+                to="/password/set_password/:id"
                 className="flex justify-evenly w-full p-2 "
               >
                 <span className="text-black font-semibold text-[14px]">
