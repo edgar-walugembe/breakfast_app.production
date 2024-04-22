d6e717fed53cceaecc93cf3d55c10c0be5381d13
windows <edgar.walugembe.upti@gmail.com>
2024-04-22 15:51:23 +0300

    production 0001

--- server/database/models/finances.js
+++ server/database/models/finances.js
@@ -8,13 +8,42 @@ module.exports = (sequelize, DataTypes) => {
   }
   Finances.init(
     {
-      recordId: DataTypes.INTEGER,
-      name: DataTypes.STRING,
-      currentDebt: DataTypes.INTEGER,
-      paidDebt: DataTypes.INTEGER,
-      balance: DataTypes.INTEGER,
-      status: DataTypes.STRING,
-      adminId: DataTypes.INTEGER,
+      recordId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      currentDebt: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      paidDebt: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      balance: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.ENUM("cleared", "pending", "declined"),
+        allowNull: false,
+        validate: {
+          isIn: {
+            args: [["cleared", "pending", "declined"]],
+            msg: "Invalid Status",
+          },
+        },
+      },
+      adminId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
     {
       sequelize,
